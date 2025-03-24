ee3c871d72d76f3ec529a175b5c9775ae8149654
DeevS <mygrayroom@gmail.com>
2025-03-24 17:18:20 +0300

    Spring for Kafka. Kafka Producer. Product Service

---
+++ ProductsMicroservice/src/main/java/com/appsdeveloperblog/ws/products/service/ProductService.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.ws.products.service;
+
+import com.appsdeveloperblog.ws.products.dto.ProductCreateDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    public String createProduct(ProductCreateDto dto) {
+        return null;
+    }
+
+}
