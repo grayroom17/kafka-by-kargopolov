8c9f2a053bdf21b6cde1f1c14455605ad10bbfd8
DeevS <mygrayroom@gmail.com>
2025-03-25 20:59:24 +0300

    Spring for Kafka. KafkaTemplate bean config

--- ProductsMicroservice/src/main/java/com/appsdeveloperblog/ws/products/config/KafkaConfig.java
+++ ProductsMicroservice/src/main/java/com/appsdeveloperblog/ws/products/config/KafkaConfig.java
@@ -1,9 +1,17 @@
 package com.appsdeveloperblog.ws.products.config;
 
+import com.appsdeveloperblog.ws.products.event.ProductCreatedEvent;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.Map;
 
@@ -11,6 +19,12 @@ import static com.appsdeveloperblog.ws.products.config.KafkaTopics.PRODUCT_CRATE
 
 @Configuration
 public class KafkaConfig {
+
+    private final KafkaProperties properties;
+
+    public KafkaConfig(KafkaProperties properties) {
+        this.properties = properties;
+    }
 
     @Bean
     NewTopic createTopic() {
@@ -21,6 +35,23 @@ public class KafkaConfig {
                 .replicas(3)
                 .configs(configs)
                 .build();
+    }
+
+    private Map<String, Object> producerConfigs() {
+        Map<String, Object> configs = properties.buildProducerProperties();
+        configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return configs;
+    }
+
+    @Bean
+    public ProducerFactory<String, ProductCreatedEvent> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfigs());
+    }
+
+    @Bean
+    KafkaTemplate<String, ProductCreatedEvent> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
     }
 
 }
