8e0c218a0cfe9ebac241448a2441960b2dbd68b9
DeevS <mygrayroom@gmail.com>
2025-03-27 19:53:28 +0300

    Spring for Kafka. Consumer. KafkaListener

---
+++ EmailNotificationMicroservice/src/main/java/com/appsdeveloperblog/ws/emailnotification/handler/ProductCreatedEventHandler.java
@@ -0,0 +1,22 @@
+package com.appsdeveloperblog.ws.emailnotification.handler;
+
+import com.appsdeveloperblog.ws.core.event.ProductCreatedEvent;
+import com.appsdeveloperblog.ws.core.config.KafkaTopics;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaHandler;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@KafkaListener(topics = KafkaTopics.PRODUCT_CRATED_EVENTS_TOPIC)
+public class ProductCreatedEventHandler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProductCreatedEventHandler.class);
+
+    @KafkaHandler
+    public void handle(ProductCreatedEvent productCreatedEvent) {
+        LOGGER.info("***** Received product created event: {}", productCreatedEvent);
+    }
+
+}
