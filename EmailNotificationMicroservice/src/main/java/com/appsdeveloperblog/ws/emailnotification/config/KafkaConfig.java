8ed3102e22abf224188cf424d26a67e2966cc361
DeevS <mygrayroom@gmail.com>
2025-03-27 20:52:03 +0300

    Spring for Kafka. Consumer. Bean Configuration

--- EmailNotificationMicroservice/src/main/java/com/appsdeveloperblog/ws/emailnotification/config/KafkaConfig.java
+++ EmailNotificationMicroservice/src/main/java/com/appsdeveloperblog/ws/emailnotification/config/KafkaConfig.java
@@ -1,10 +1,11 @@
 package com.appsdeveloperblog.ws.emailnotification.config;
 
-import com.fasterxml.jackson.databind.deser.std.StringDeserializer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
@@ -19,8 +20,15 @@ public class KafkaConfig {
         Map<String, Object> config = kafkaProperties.buildConsumerProperties();
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
-        config.put(JsonDeserializer.TRUSTED_PACKAGES, "com.appsdeveloperblog.ws.core");
+        config.put(JsonDeserializer.TRUSTED_PACKAGES, "com.appsdeveloperblog.ws.core.*");
         return new DefaultKafkaConsumerFactory<>(config);
+    }
+
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory(ConsumerFactory<String, Object> consumerFactory) {
+        ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
+        return factory;
     }
 
 }
