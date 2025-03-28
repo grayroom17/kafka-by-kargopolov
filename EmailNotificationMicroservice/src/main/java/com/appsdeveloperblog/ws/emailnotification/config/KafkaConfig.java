9c863687298a44b1a2726d1b4798939b0d6d9617
DeevS <mygrayroom@gmail.com>
2025-03-28 19:30:15 +0300

    Spring for Kafka. Consumer. Handle Deserializer Errors

--- EmailNotificationMicroservice/src/main/java/com/appsdeveloperblog/ws/emailnotification/config/KafkaConfig.java
+++ EmailNotificationMicroservice/src/main/java/com/appsdeveloperblog/ws/emailnotification/config/KafkaConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.Map;
@@ -19,7 +20,8 @@ public class KafkaConfig {
     public ConsumerFactory<String, Object> consumerFactory(KafkaProperties kafkaProperties) {
         Map<String, Object> config = kafkaProperties.buildConsumerProperties();
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+        config.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class);
         config.put(JsonDeserializer.TRUSTED_PACKAGES, "com.appsdeveloperblog.ws.core.*");
         return new DefaultKafkaConsumerFactory<>(config);
     }
