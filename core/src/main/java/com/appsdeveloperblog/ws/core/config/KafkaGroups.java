da16a41099258f16818abe2b3c3fab533846cce7
DeevS <mygrayroom@gmail.com>
2025-04-01 21:04:54 +0300

    Spring for Kafka. Consumer. Consumer Groups

---
+++ core/src/main/java/com/appsdeveloperblog/ws/core/config/KafkaGroups.java
@@ -0,0 +1,8 @@
+package com.appsdeveloperblog.ws.core.config;
+
+public class KafkaGroups {
+    private KafkaGroups() {
+    }
+
+    public static final String PRODUCT_CRATED_EVENTS_GROUP = "product-crated-events";
+}
