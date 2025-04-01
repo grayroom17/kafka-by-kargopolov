2f35718905719f1a5c4c97812aa32b118934d8bf
DeevS <mygrayroom@gmail.com>
2025-04-01 20:14:18 +0300

    Spring for Kafka. Consumer. Handle Retryable Exceptions. Interval and max Attempts

--- EmailNotificationMicroservice/src/main/java/com/appsdeveloperblog/ws/emailnotification/handler/ProductCreatedEventHandler.java
+++ EmailNotificationMicroservice/src/main/java/com/appsdeveloperblog/ws/emailnotification/handler/ProductCreatedEventHandler.java
@@ -3,11 +3,18 @@ package com.appsdeveloperblog.ws.emailnotification.handler;
 import com.appsdeveloperblog.ws.core.config.KafkaTopics;
 import com.appsdeveloperblog.ws.core.event.ProductCreatedEvent;
 import com.appsdeveloperblog.ws.emailnotification.error.NotRetryableException;
+import com.appsdeveloperblog.ws.emailnotification.error.RetryableException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestTemplate;
+
+import static org.springframework.http.HttpMethod.GET;
 
 @Component
 @KafkaListener(topics = KafkaTopics.PRODUCT_CRATED_EVENTS_TOPIC)
@@ -15,10 +22,31 @@ public class ProductCreatedEventHandler {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ProductCreatedEventHandler.class);
 
+    private final RestTemplate restTemplate;
+
+    public ProductCreatedEventHandler(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     @KafkaHandler
     public void handle(ProductCreatedEvent productCreatedEvent) {
-        if (true) throw new NotRetryableException("An error took place. No need to consume this message again.");
         LOGGER.info("***** Received product created event: {}", productCreatedEvent.getTitle());
+        try {
+            String url = "http://localhost:8082/response/200";
+            ResponseEntity<String> response = restTemplate.exchange(url, GET, null, String.class);
+            if (response.getStatusCode().is2xxSuccessful()) {
+                LOGGER.info("***** Received response from a remote service: {}", response.getBody());
+            }
+        } catch (ResourceAccessException e) {
+            LOGGER.error(e.getMessage());
+            throw new RetryableException(e);
+        } catch (HttpServerErrorException e) {
+            LOGGER.error(e.getMessage());
+            throw new NotRetryableException(e);
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+            throw new NotRetryableException(e);
+        }
     }
 
 }
