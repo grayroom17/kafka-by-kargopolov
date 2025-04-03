304dcfb701c82049ebe679ea41c6e856a138713b
DeevS <mygrayroom@gmail.com>
2025-04-03 19:03:01 +0300

    Spring for Kafka. Consumer. Entity and Repository

---
+++ EmailNotificationMicroservice/src/main/java/com/appsdeveloperblog/ws/emailnotification/domain/repository/ProcessedEventRepository.java
@@ -0,0 +1,9 @@
+package com.appsdeveloperblog.ws.emailnotification.domain.repository;
+
+import com.appsdeveloperblog.ws.emailnotification.domain.entity.ProcessedEvent;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProcessedEventRepository extends JpaRepository<ProcessedEvent, Long> {
+}
