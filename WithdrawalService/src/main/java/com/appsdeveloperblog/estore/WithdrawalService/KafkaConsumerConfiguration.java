614fd63d2a4da266f5440785a791604272094d99
DeevS <mygrayroom@gmail.com>
2025-04-07 21:07:00 +0300

    Spring for Kafka. Kafka Transactions. Read only read commited messages

--- WithdrawalService/src/main/java/com/appsdeveloperblog/estore/WithdrawalService/KafkaConsumerConfiguration.java
+++ WithdrawalService/src/main/java/com/appsdeveloperblog/estore/WithdrawalService/KafkaConsumerConfiguration.java
@@ -1,18 +1,15 @@
 package com.appsdeveloperblog.estore.WithdrawalService;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import com.appsdeveloperblog.ws.core.error.NotRetryableException;
 import com.appsdeveloperblog.ws.core.error.RetryableException;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.IsolationLevel;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
@@ -26,58 +23,54 @@ import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 import org.springframework.util.backoff.FixedBackOff;
 
+import java.util.Map;
+
 @Configuration
 public class KafkaConsumerConfiguration {
 
-	@Autowired
-	Environment environment;
+    private final KafkaProperties kafkaProperties;
 
-	@Bean
-	ConsumerFactory<String, Object> consumerFactory() {
-		Map<String, Object> config = new HashMap<>();
+    public KafkaConsumerConfiguration(KafkaProperties kafkaProperties) {
+        this.kafkaProperties = kafkaProperties;
+    }
 
-		config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
-				environment.getProperty("spring.kafka.consumer.bootstrap-servers"));
-		config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-		config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
-		config.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class);
-		config.put(ConsumerConfig.GROUP_ID_CONFIG, environment.getProperty("spring.kafka.consumer.group-id"));
-		config.put(JsonDeserializer.TRUSTED_PACKAGES,
-				environment.getProperty("spring.kafka.consumer.properties.spring.json.trusted.packages"));
+    @Bean
+    ConsumerFactory<String, Object> consumerFactory() {
+        Map<String, Object> config = kafkaProperties.buildConsumerProperties();
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+        config.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class);
+        config.put(ConsumerConfig.ISOLATION_LEVEL_CONFIG, IsolationLevel.READ_COMMITTED);
+        return new DefaultKafkaConsumerFactory<>(config);
+    }
 
-		return new DefaultKafkaConsumerFactory<>(config);
-	}
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory(
+            ConsumerFactory<String, Object> consumerFactory, KafkaTemplate<String, Object> kafkaTemplate) {
 
-	@Bean
-	ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory(
-			ConsumerFactory<String, Object> consumerFactory, KafkaTemplate<String, Object> kafkaTemplate) {
+        ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
 
-		ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
-		factory.setConsumerFactory(consumerFactory);
+        DefaultErrorHandler errorHandler = new DefaultErrorHandler(new DeadLetterPublishingRecoverer(kafkaTemplate),
+                new FixedBackOff(5000, 3));
+        errorHandler.addNotRetryableExceptions(NotRetryableException.class);
+        errorHandler.addRetryableExceptions(RetryableException.class);
+        factory.setCommonErrorHandler(errorHandler);
 
-		DefaultErrorHandler errorHandler = new DefaultErrorHandler(new DeadLetterPublishingRecoverer(kafkaTemplate),
-				new FixedBackOff(5000, 3));
-		errorHandler.addNotRetryableExceptions(NotRetryableException.class);
-		errorHandler.addRetryableExceptions(RetryableException.class);
-		factory.setCommonErrorHandler(errorHandler);
+        return factory;
+    }
 
-		return factory;
-	}
+    @Bean
+    KafkaTemplate<String, Object> kafkaTemplate(ProducerFactory<String, Object> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
+    }
 
-	@Bean
-	KafkaTemplate<String, Object> kafkaTemplate(ProducerFactory<String, Object> producerFactory) {
-		return new KafkaTemplate<>(producerFactory);
-	}
-
-	@Bean
-	ProducerFactory<String, Object> producerFactory() {
-		Map<String, Object> config = new HashMap<>();
-		config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
-				environment.getProperty("spring.kafka.consumer.bootstrap-servers"));
-		config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-		config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-
-		return new DefaultKafkaProducerFactory<>(config);
-	}
+    @Bean
+    ProducerFactory<String, Object> producerFactory() {
+        Map<String, Object> config = kafkaProperties.buildProducerProperties();
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return new DefaultKafkaProducerFactory<>(config);
+    }
 
 }
