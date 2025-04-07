d9344a0bfe5c57cf7e9c9af216af7f49407ad87a
DeevS <mygrayroom@gmail.com>
2025-04-07 16:00:11 +0300

    Spring for Kafka. Kafka Transactions. Enable Kafka Transaction via Java code

--- TransferService/src/main/java/com/appsdeveloperblog/estore/transfers/KafkaConfig.java
+++ TransferService/src/main/java/com/appsdeveloperblog/estore/transfers/KafkaConfig.java
@@ -1,6 +1,7 @@
 package com.appsdeveloperblog.estore.transfers;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +10,7 @@ import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.transaction.KafkaTransactionManager;
 
 import java.util.Map;
 
@@ -19,6 +21,8 @@ public class KafkaConfig {
     private String withdrawTopicName;
     @Value("deposit-money-topic")
     private String depositTopicName;
+    @Value("${spring.kafka.producer.transaction-id-prefix}")
+    private String transactionIdPrefix;
 
     private final KafkaProperties kafkaProperties;
 
@@ -27,7 +31,9 @@ public class KafkaConfig {
     }
 
     public Map<String, Object> producerConfigs() {
-        return kafkaProperties.buildProducerProperties();
+        Map<String, Object> config = kafkaProperties.buildProducerProperties();
+        config.put(ProducerConfig.TRANSACTIONAL_ID_CONFIG, transactionIdPrefix);
+        return config;
     }
 
     @Bean
@@ -38,6 +44,11 @@ public class KafkaConfig {
     @Bean
     KafkaTemplate<String, Object> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
+    KafkaTransactionManager<String, Object> kafkaTransactionManager(ProducerFactory<String, Object> producerFactory) {
+        return new KafkaTransactionManager<>(producerFactory);
     }
 
     @Bean
