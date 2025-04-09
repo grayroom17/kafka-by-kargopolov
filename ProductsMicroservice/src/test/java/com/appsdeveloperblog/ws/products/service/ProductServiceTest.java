ba0f266c08f541968199b7f2dddfa8b24312e8e2
DeevS <mygrayroom@gmail.com>
2025-04-09 19:57:16 +0300

    Spring for Kafka. Integration tests. Producer. Test method

--- ProductsMicroservice/src/test/java/com/appsdeveloperblog/ws/products/service/ProductServiceTest.java
+++ ProductsMicroservice/src/test/java/com/appsdeveloperblog/ws/products/service/ProductServiceTest.java
@@ -1,16 +1,110 @@
 package com.appsdeveloperblog.ws.products.service;
 
+import com.appsdeveloperblog.ws.core.event.ProductCreatedEvent;
+import com.appsdeveloperblog.ws.products.dto.ProductCreateDto;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.listener.KafkaMessageListenerContainer;
+import org.springframework.kafka.listener.MessageListener;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.ContainerTestUtils;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.math.BigDecimal;
+import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+import static com.appsdeveloperblog.ws.core.config.KafkaTopics.PRODUCT_CRATED_EVENTS_TOPIC;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DirtiesContext
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @ActiveProfiles("test")
 @EmbeddedKafka(partitions = 3, count = 3, controlledShutdown = true)
 @SpringBootTest(properties = "spring.kafka.producer.bootstrap-servers=${spring.embedded.kafka.brokers}")
-class ProductServiceIT {
+class ProductServiceTest {
+
+    @Autowired
+    private ProductService productService;
+    @Autowired
+    private EmbeddedKafkaBroker embeddedKafkaBroker;
+    @Value("${spring.kafka.consumer.group-id}")
+    private String groupId;
+    @Value("${spring.kafka.consumer.properties.spring.json.trusted.packages}")
+    private String trustedPackages;
+    @Value("${spring.kafka.consumer.auto-offset-reset}")
+    private String autoOffsetReset;
+
+    private KafkaMessageListenerContainer<String, ProductCreatedEvent> container;
+    private BlockingQueue<ConsumerRecord<String, ProductCreatedEvent>> records;
+
+    @BeforeAll
+    void setUp() {
+        DefaultKafkaConsumerFactory<Object, Object> consumerFactory = new DefaultKafkaConsumerFactory<>(getConsumerProperties());
+        ContainerProperties containerProperties = new ContainerProperties(PRODUCT_CRATED_EVENTS_TOPIC);
+        container = new KafkaMessageListenerContainer<>(consumerFactory, containerProperties);
+        records = new LinkedBlockingQueue<>();
+        container.setupMessageListener((MessageListener<String, ProductCreatedEvent>) records::add);
+        container.start();
+        ContainerTestUtils.waitForAssignment(container, embeddedKafkaBroker.getPartitionsPerTopic());
+    }
+
+    @AfterAll
+    void tearDown() {
+        container.stop();
+    }
+
+    @Test
+    void createProduct_whenGivenValidProductDetails_thenSendKafkaMessage() throws Exception {
+        //given
+        String title = "iPhone 11";
+        BigDecimal price = new BigDecimal(600);
+        int quantity = 1;
+
+        ProductCreateDto dto = new ProductCreateDto();
+        dto.setTitle(title);
+        dto.setPrice(price);
+        dto.setQuantity(quantity);
+
+        //when
+        productService.createProduct(dto);
+
+        //then
+        ConsumerRecord<String, ProductCreatedEvent> message = records.poll(3000, TimeUnit.MILLISECONDS);
+        assertNotNull(message);
+        assertNotNull(message.key());
+        ProductCreatedEvent productCreatedEvent = message.value();
+        assertEquals(title, productCreatedEvent.getTitle());
+        assertEquals(price, productCreatedEvent.getPrice());
+        assertEquals(quantity, productCreatedEvent.getQuantity());
+    }
+
+    private Map<String, Object> getConsumerProperties() {
+        return Map.of(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, embeddedKafkaBroker.getBrokersAsString(),
+                ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
+                ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class,
+                ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class,
+                ConsumerConfig.GROUP_ID_CONFIG, groupId,
+                JsonDeserializer.TRUSTED_PACKAGES, trustedPackages,
+                ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, autoOffsetReset);
+    }
+
 
 }
