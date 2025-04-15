b0b8b444c18137b039bd1484d56999b2b4b8d8d4
DeevS <mygrayroom@gmail.com>
2025-04-15 21:31:05 +0300

    Spring for Kafka. Saga. OrderCreatedEvent and Topic

--- saga-pattern-spring-boot-demo/orders-service/src/main/java/com/appsdeveloperblog/orders/config/KafkaConfig.java
+++ saga-pattern-spring-boot-demo/orders-service/src/main/java/com/appsdeveloperblog/orders/config/KafkaConfig.java
@@ -1,16 +1,33 @@
 package com.appsdeveloperblog.orders.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
 @Configuration
 public class KafkaConfig {
 
+    @Value("${orders.events.topic.name}")
+    private String orderEventsTopicName;
+    private static final Integer DEFAULT_PARTITION = 3;
+    private static final Integer DEFAULT_REPLICATION_FACTOR = 3;
+
     @Bean
     KafkaTemplate<String, Object> kafkaTemplate(ProducerFactory<String, Object> producerFactory) {
         return new KafkaTemplate<>(producerFactory);
+    }
+
+    @Bean
+    NewTopic createOrderCreatedTopic() {
+        return TopicBuilder
+                .name(orderEventsTopicName)
+                .partitions(DEFAULT_PARTITION)
+                .replicas(DEFAULT_REPLICATION_FACTOR)
+                .build();
     }
 
 }
--- saga-pattern-spring-boot-demo/orders-service/src/main/java/com/appsdeveloperblog/orders/service/OrderServiceImpl.java
+++ saga-pattern-spring-boot-demo/orders-service/src/main/java/com/appsdeveloperblog/orders/service/OrderServiceImpl.java
@@ -1,17 +1,27 @@
 package com.appsdeveloperblog.orders.service;
 
 import com.appsdeveloperblog.core.dto.Order;
+import com.appsdeveloperblog.core.events.OrderCreatedEvent;
 import com.appsdeveloperblog.core.types.OrderStatus;
 import com.appsdeveloperblog.orders.dao.jpa.entity.OrderEntity;
 import com.appsdeveloperblog.orders.dao.jpa.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderServiceImpl implements OrderService {
-    private final OrderRepository orderRepository;
 
-    public OrderServiceImpl(OrderRepository orderRepository) {
+    private final OrderRepository orderRepository;
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+    private final String orderEventsTopicName;
+
+    public OrderServiceImpl(OrderRepository orderRepository,
+                            KafkaTemplate<String, Object> kafkaTemplate,
+                            @Value("${orders.events.topic.name}") String orderEventsTopicName) {
         this.orderRepository = orderRepository;
+        this.kafkaTemplate = kafkaTemplate;
+        this.orderEventsTopicName = orderEventsTopicName;
     }
 
     @Override
@@ -22,6 +32,15 @@ public class OrderServiceImpl implements OrderService {
         entity.setProductQuantity(order.getProductQuantity());
         entity.setStatus(OrderStatus.CREATED);
         orderRepository.save(entity);
+
+        OrderCreatedEvent orderCreatedEvent = new OrderCreatedEvent(
+                entity.getId(),
+                entity.getCustomerId(),
+                entity.getProductId(),
+                entity.getProductQuantity()
+        );
+
+        kafkaTemplate.send(orderEventsTopicName, orderCreatedEvent);
 
         return new Order(
                 entity.getId(),
